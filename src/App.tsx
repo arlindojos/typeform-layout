1457899d5aa9096b4f1239fc80b71d36fa23a31d
Arlindo jose <arlindojosboa@gmail.com>
2020-09-22 00:48:46 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import GlobalStyles from './styles/globalStyles';
+import { Main, Section, Sticky } from './styles';
+import FirstAndSecond from './components/FirstAndSecond';
+import Header from './components/Header';
+
+function App() {
+  useEffect(() => {
+    window.scrollTo({ top: 0})
+  }, [])
+  return (
+    <>
+      <Header />
+      <Main style={{height: '1610vh'}}>
+        <Section style={{height: '72.7%'}}>
+          <FirstAndSecond />
+        </Section>
+
+        <Section style={{height: '9.7%'}}>
+          <Sticky className="third" />
+        </Section>
+
+        <Section style={{height: '10.1%'}}>
+          <Sticky className="fourth" />
+        </Section>
+      </Main>
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
